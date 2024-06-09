e7ac88b91cb150ddda00a5b30a25dc8dc36c87a5
Anisa <111259581+anisamsrh@users.noreply.github.com>
2024-06-09 07:25:09 +0700

    Create p4.cpp

---
+++ p4.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang {
+    public :
+        int xmin, xmax, ymin, ymax;
+        
+        PersegiPanjang(int x, int y, int width, int height) {
+            xmin = x - width/2;
+            xmax = x + width/2;
+            ymin = y - height/2;
+            ymax = y + height/2;
+        }
+        
+        PersegiPanjang operator + (const PersegiPanjang& lain) {
+            if (this->xmin >= lain.xmin && this->xmin <= lain.xmax) {
+                int width = this->xmax - lain.xmin;
+                int x = lain.xmin + width/2;
+                if (this->ymin >= lain.ymin && this->ymin <= lain.ymax) {
+                    int height = this->ymax - lain.ymin;
+                    int y = lain.ymin + height/2;
+                    return PersegiPanjang(x, y, width, height);
+                } else if (this->ymax >= lain.ymin && this->ymax <= lain.ymax) {
+                    int height = lain.ymax - this->ymin;
+                    int y = this->ymin + height/2;
+                    return PersegiPanjang(x, y, width, height);
+                }
+            } else if (this->xmax >= lain.xmin && this->xmax <= lain.xmax) {
+                int width = lain.xmax - this->xmin;
+                int x = this->xmin + width/2;
+                if (this->ymin >= lain.ymin && this->ymin <= lain.ymax) {
+                    int height = this->ymax - lain.ymin;
+                    int y = lain.ymin + height/2;
+                    return PersegiPanjang(x, y, width, height);
+                } else if (this->ymax >= lain.ymin && this->ymax <= lain.ymax) {
+                    int height = lain.ymax - this->ymin;
+                    int y = this->ymin + height/2;
+                    return PersegiPanjang(x, y, width, height);
+                }
+            }
+        }
+        
+        PersegiPanjang operator ++ (int u) {
+            this->xmax += (this->xmax - this->xmin);
+            this->ymax += (this->ymax - this->ymin);
+            // return;
+        }
+        
+        PersegiPanjang operator -- (int u) {
+            this->xmax -= (this->xmax - this->xmin)/2;
+            this->ymax -= (this->ymax - this->ymin)/2;
+            // return;
+        }
+        
+        bool operator == (const PersegiPanjang& lain) const {
+            if (this->xmin >= lain.xmin && this->xmin <= lain.xmax) {
+                if (this->ymin >= lain.ymin && this->ymin <= lain.ymax) {
+                    return true;
+                } else if (this->ymax >= lain.ymin && this->ymax <= lain.ymax) {
+                    return true;
+                }
+            } else if (this->xmax >= lain.xmin && this->xmax <= lain.xmax) {
+                if (this->ymin >= lain.ymin && this->ymin <= lain.ymax) {
+                    return true;
+                } else if (this->ymax >= lain.ymin && this->ymax <= lain.ymax) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+        
+};
+
+int main()
+{
+    PersegiPanjang biru(10, 10, 4, 6);
+    PersegiPanjang merah(12, 10, 4, 6);
+    cout << biru.xmax - biru.xmin << endl;
+    biru++;
+    cout << biru.xmax - biru.xmin << endl;
+    biru--;
+    cout << biru.xmax - biru.xmin << endl;
+    if (merah == biru) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
